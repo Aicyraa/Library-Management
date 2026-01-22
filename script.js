400f2c3be657d39811a8f72253fa4452f899265f
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-22 08:14:08 +0800

    refactor ui functions

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+import { toggleSidebar, toggleForm } from "./scripts/ui.util.js";
 
+const UIbtns = {
+   overlay: document.querySelector(".overlay"),
+   sidebarBtns: document.querySelectorAll('.sidebar-btn'),
+   modalAddBtn: document.getElementById('book-navbar-add'),
+   modalCloseBtn: document.getElementById("modal-close"),
+   modalCancelBtn: document.getElementById("modal-cancel"),
 
-
+};
 
 (function(){
-   
+   UIbtns.sidebarBtns.forEach(btns => btns.addEventListener("click", toggleSidebar));
+   UIbtns.modalAddBtn.addEventListener("click", toggleForm)
+   UIbtns.modalCloseBtn.addEventListener("click", toggleForm)
+   UIbtns.modalCancelBtn.addEventListener("click", toggleForm)
+   UIbtns.overlay.addEventListener("click", toggleForm)
+
 })();
